89e34294f2591d57bea957f2c90291a10e76ab8f
Ariane <ariane.fairlie@gmail.com>
2020-10-21 13:07:45 -0400

    emoji animation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 import Layout from '../components/layout'
 
 const Content = styled.div`
@@ -9,10 +9,46 @@ const Content = styled.div`
   justify-content: center;
   align-items: center;
   padding: 1rem;
+  z-index: 1000;
 
   h1 {
     font-size: 5em;
   }
+
+  p {
+    text-align: center;
+  }
+`
+
+const Comp = styled.div`
+  position: relative;
+  font-size:calc(50px + 6vw);
+  z-index: 900;
+`
+const rotate = keyframes`
+  from {
+    transform: scale(-1, 1) rotate(0deg);
+  }
+
+  50% {
+    transform: scale(-1, 1) rotate(20deg);
+  }
+
+  to {
+    transform: scale(-1, 1) rotate(0deg);
+  }
+`;
+
+const Hello = styled.div`
+  position: absolute;
+  font-size:calc(50px + 6vw);
+  z-index: 900;
+  right: 12vw;
+  top: calc(58% - 23vmin);
+  transform: scale(-1, 1);
+
+  animation: ${rotate} 0.4s linear 3;
+  animation-delay: 1.5s;
 `
 
 export default function Home() {
@@ -22,9 +58,10 @@ export default function Home() {
           <title>ariane's portfolio</title>
           <link rel="icon" href="/logo.jpg" />
         </Head>
-        
+        <Hello>👋</Hello>
         <Content>
           <h1>Ariane Fairlie</h1>
+          <Comp>💻</Comp>
           <h2>
             Full Stack Developer
           </h2>
